cc7a5b4f453bfdb677e7a46b8f50527b2dcf5e74
Lucjan Suski <lucjansuski@gmail.com>
2016-02-09 13:04:50 +0100

    Update config to work with new libs

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,7 +1,7 @@
 /** @jsx React.DOM */
 
 var React = window.React = require('react'),
-    Folders = require('./components/folders/Folders'),
+    Folders = require('./components/folders/folders'),
     mountNode = document.getElementById("app");
 
 var FoldersApp = React.createClass({
